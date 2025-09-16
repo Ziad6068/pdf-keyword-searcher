77aba89daf16c7dcd318526cdf4ddc4f7e27cb8b
Ziad6068 <ziadk6068@gmail.com>
2025-09-16 10:56:53 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Folder with your PDFs
+FOLDER_PATH = r"write your path folder that contains the multiple pdf that you want to search keywords in them"
+
+# change the Keywords to the keywords that fits your search
+KEYWORDS = ["bloc", "bem", "nollusques", "cryptocristal", "petites"]
+
+# Output file
+OUTPUT_FILE = "results.txt"
